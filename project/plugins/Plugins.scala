21bc4a7e3e14eee1bcfecd1df630db1ef4b0690d
Robey Pointer <robeypointer@gmail.com>
2010-05-04 16:21:05 -0700

    use newer standard-project.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,5 +1,7 @@
 import sbt._
 
+
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val defaultProject = "com.twitter" % "standard-project" % "0.1"
+  val twitterNest = "com.twitter" at "http://www.lag.net/nest"
+  val defaultProject = "com.twitter" % "standard-project" % "0.3"
 }
