3c9607b021e695a48070aee3bf1b57050785f91d
Robey Pointer <robeypointer@gmail.com>
2010-05-04 21:34:23 -0700

    bump to 1.5.3.

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -5,4 +5,18 @@ import com.twitter.sbt.StandardProject
 class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) {
   val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
+
+//  val publishTo = Resolver.sftp("green.lag.net", "green.lag.net", "/web/repo")
+
+  override def pomExtra =
+    <licenses>
+      <license>
+        <name>Apache 2</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+
+  Credentials(Path.userHome / ".ivy2" / "credentials", log)
+  val publishTo = "nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
 }
