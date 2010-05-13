cd0e77c138d2e1df391098bc998872e6f8921d18
Robey Pointer <robeypointer@gmail.com>
2010-05-13 15:41:05 -0700

    can't mark this as test.

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -3,7 +3,7 @@ import com.twitter.sbt._
 
 
 class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) {
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
+  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
 
   override def pomExtra =
