3986081a81d7308b0406465afd5b1449f80a7936
Ismael Juma <ismael@juma.me.uk>
2010-08-06 06:35:38 +0100

    Set build.scala.versions to 2.8.0 and upgrade specs to 1.6.5.

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -3,7 +3,7 @@ import com.twitter.sbt._
 
 
 class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) {
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
 
   override def pomExtra =
