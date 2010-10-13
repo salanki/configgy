a4aaf08ba8600eb850db5922c1210e1422dc83ce
Robey Pointer <robeypointer@gmail.com>
2010-10-13 13:59:53 -0700

    fix merge conflict.

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -1,14 +1,10 @@
 import sbt._
 import com.twitter.sbt._
 
-
-class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) {
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5"
-  val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
 class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
-  val json = "com.twitter" % "json" % "1.1.3"
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3" % "provided->default"
+  val json = "com.twitter" % "json" % "1.1.3"
 
   override def pomExtra =
     <licenses>
