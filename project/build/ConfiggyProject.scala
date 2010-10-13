f99866e6ce413bedecae0fcc06362aaead4ead99
Robey Pointer <robeypointer@gmail.com>
2010-10-13 14:11:56 -0700

    use json from 2.8.0.

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -4,7 +4,10 @@ import com.twitter.sbt._
 class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.5"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3" % "provided->default"
-  val json = "com.twitter" % "json" % "1.1.3"
+  val json = buildScalaVersion match {
+    case "2.7.7" => "com.twitter" % "json" % "1.1.3"
+    case _ => "com.twitter" %% "json" % "2.1.4"
+  }
 
   override def pomExtra =
     <licenses>
