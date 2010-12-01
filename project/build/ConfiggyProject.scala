0d1c1ca857ee9ea23eafb909921feb5314218c08
Robey Pointer <robeypointer@gmail.com>
2010-11-30 17:41:05 -0800

    remove vscaladoc from merge.

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -3,7 +3,6 @@ import com.twitter.sbt._
 
 class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.5"
-  val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3" % "provided->default"
   val json = buildScalaVersion match {
     case "2.7.7" => "com.twitter" % "json" % "1.1.3"
     case _ => "com.twitter" %% "json" % "2.1.4"
