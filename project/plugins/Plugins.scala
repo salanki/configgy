91f8254a71969d0ae3bc0e7e2f2e4e04f905fcb5
Kyle Maxwell <kmaxwell@twitter.com>
2010-12-01 11:17:46 -0800

    upgrade inline

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,12 +1,7 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-<<<<<<< HEAD
   val scalaTools = "scala-tools.org" at "http://scala-tools.org/repo-releases/"
-  val lagNet = "twitter.com" at "http://www.lag.net/repo/"
-  val defaultProject = "com.twitter" % "standard-project" % "0.7.16"
-=======
+  val defaultProject = "com.twitter" % "standard-project" % "0.7.17"
   val twitter = "twitter.com" at "http://maven.twttr.com"
-  val defaultProject = "com.twitter" % "standard-project" % "0.7.10"
->>>>>>> f9c1639208e51f0b9a0db65f396ad37310292aae
 }
