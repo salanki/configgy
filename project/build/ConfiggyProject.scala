9aa6993cf6e4877ad3a83a3fbd5a267eab16b19c
Ismael Juma <ismael@juma.me.uk>
2011-06-10 14:53:22 +0100

    Configure build for Scala 2.9.0-1.

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -2,7 +2,7 @@ import sbt._
 import com.twitter.sbt._
 
 class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
-  val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test"
+  val specs = "org.scala-tools.testing" % "specs_2.9.0-1" % "1.6.8" % "test"
   val json = buildScalaVersion match {
     case "2.7.7" => "com.twitter" % "json" % "1.1.7"
     case _ => "com.twitter" % "json_2.8.1" % "2.1.6"
