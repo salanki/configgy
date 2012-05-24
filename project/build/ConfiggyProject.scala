f605ceb011d765606ad7e5ff850c8d4989608259
Robey Pointer <robeypointer@gmail.com>
2012-05-24 14:10:36 -0700

    cross build against scala 2.8.1 and 2.9.2

--- project/build/ConfiggyProject.scala
+++ project/build/ConfiggyProject.scala
@@ -2,9 +2,15 @@ import sbt._
 import com.twitter.sbt._
 
 class ConfiggyProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
-  val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test"
+  override def disableCrossPaths = false
+
+  val specs = buildScalaVersion match {
+    case "2.8.1" => "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test"
+    case "2.9.2" => "org.scala-tools.testing" % "specs_2.9.1" % "1.6.9" % "test"
+  }
   val json = buildScalaVersion match {
     case "2.7.7" => "com.twitter" % "json" % "1.1.7"
+    case "2.8.1" => "com.twitter" % "json_2.8.1" % "2.1.6"
     case _ => "com.twitter" % "json_2.8.1" % "2.1.6"
   }
 
