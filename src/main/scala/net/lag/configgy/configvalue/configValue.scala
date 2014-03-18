601aa9281a4e81b6fee47f9a8c33a5477230746d
Peter Salanki <peter@salanki.st>
2014-03-18 02:19:31 -0400

    Made custom equals methods so that a List with a single element equals the same element without being wrapped in a list. Added MapProxy trait to ConfigObject to enable Map methods directly on the instance

--- src/main/scala/net/lag/configgy/configvalue/configValue.scala
+++ src/main/scala/net/lag/configgy/configvalue/configValue.scala
@@ -1,7 +1,7 @@
 package net.lag.configgy
 package configvalue
 
-import scala.collection.immutable.TreeMap
+import scala.collection.immutable.{ TreeMap, MapProxy }
 import net.lag.extensions._
 
 /**
@@ -17,10 +17,12 @@ object ConfigObject {
   def apply() = new ConfigObject(TreeMap[String, ConfigValue]()(CaseInsensitiveOrdering))
 
 }
-case class ConfigObject[+A <: ConfigValue](entries: TreeMap[String, A]) extends ConfigValue {
+case class ConfigObject[+A <: ConfigValue](entries: TreeMap[String, A]) extends ConfigValue with MapProxy[String, A] {
   override def toString = "{" + entries.map({ case (k, v) => k + "=" + v }).mkString(" ") + "}"
+
+  def self = entries
+
   def toConfigStringList = entries match {
-    // case x if x.size < 4 => ("{" + x.map({case (k, v) => s"$k = "+v.toConfigStringList}).mkString(" ") + "}") :: Nil // ??
     case x =>
       "{" ::
         x.toList.flatMap({
@@ -35,8 +37,17 @@ case class ConfigObject[+A <: ConfigValue](entries: TreeMap[String, A]) extends 
 object ConfigList {
   def apply[A <: ConfigValue](entries: A*): ConfigList[A] = ConfigList(entries.toList)
 }
-case class ConfigList[+A <: ConfigValue](entries: List[A]) extends ConfigValue {
+case class ConfigList[+A <: ConfigValue](entries: List[A]) extends ConfigValue with Equals {
   override def toString = entries.mkString("[", ",", "]")
+
+  /**
+   * A special equals that also matches a list with a single element to the same element without a list encapsulation.
+   */
+  override def equals(other: Any) = other match {
+    case that: ConfigList[A] => this.entries == that.entries
+    case that if entries.length == 1 => entries.head == that
+    case _ => false
+  }
 
   def toConfigStringList = {
     val subLists = entries.map(_.toConfigStringList)
@@ -44,32 +55,68 @@ case class ConfigList[+A <: ConfigValue](entries: List[A]) extends ConfigValue {
       case x if x.size < 4 && subLists.find(_.size > 1) == None =>
         ("[" + subLists.map(_.head).mkString(", ") + "]") :: Nil // ??
       case x =>
-        val out = for { 
+        val out = for {
           a <- x
           ls = a.toConfigStringList
-          b <- ls.updated(ls.length-1, ls.last+",")
+          b <- ls.updated(ls.length - 1, ls.last + ",")
         } yield s"  $b"
         "[" ::
-          out.updated(out.length-1, out.last.init) :::
+          out.updated(out.length - 1, out.last.init) :::
           "]" :: Nil
     }
   }
 }
 
 case class ConfigString(value: String) extends ConfigValue {
+  /**
+   * A special equals that also matches a list with a single element to the same element without a list encapsulation.
+   */
+  override def equals(other: Any) = other match {
+    case that: ConfigString => this.value == that.value
+    case that: ConfigList[_] if that.entries.length == 1 => that.entries.head == this
+    case _ => false
+  }
+
   override def toString = "\"" + value + "\""
   def toConfigStringList = "\"" + value.quoteC + "\"" :: Nil
 }
 case class ConfigInt(value: Long) extends ConfigValue {
+  /**
+   * A special equals that also matches a list with a single element to the same element without a list encapsulation.
+   */
+  override def equals(other: Any) = other match {
+    case that: ConfigInt => this.value == that.value
+    case that: ConfigList[_] if that.entries.length == 1 => that.entries.head == this
+    case _ => false
+  }
+
   override def toString = value.toString
   def toConfigStringList = value.toString :: Nil
 }
 case class ConfigDouble(value: Double) extends ConfigValue {
+  /**
+   * A special equals that also matches a list with a single element to the same element without a list encapsulation.
+   */
+  override def equals(other: Any) = other match {
+    case that: ConfigDouble => this.value == that.value
+    case that: ConfigList[_] if that.entries.length == 1 => that.entries.head == this
+    case _ => false
+  }
+
   override def toString = value.toString
   def toConfigStringList = value.toString :: Nil
 
 }
 case class ConfigBoolean(value: Boolean) extends ConfigValue {
+  /**
+   * A special equals that also matches a list with a single element to the same element without a list encapsulation.
+   */
+  override def equals(other: Any) = other match {
+    case that: ConfigBoolean => this.value == that.value
+    case that: ConfigList[_] if that.entries.length == 1 => that.entries.head == this
+    case _ => false
+  }
+
   override def toString = value.toString
   def toConfigStringList = value.toString :: Nil
 
